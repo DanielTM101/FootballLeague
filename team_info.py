97ffa6c1b06455590af2fb3740e2f9e0757bed81
DanielTM101 <115800253+DanielTM101@users.noreply.github.com>
2023-08-18 13:09:26 +0100

    Update team_info.py

--- team_info.py
+++ team_info.py
@@ -34,11 +34,11 @@ def fetch_team_info(team_name, league, api_key):
         }
         return team_info
     except requests.exceptions.HTTPError as e:
-        return {}
+        return {}, e
     except requests.exceptions.RequestException as e:
-        return {}
+        return {}, e
     except Exception as e:
-        return {}
+        return {}, e
 
 # Function to create the SQLite database and insert user info
 def create_and_insert_user_data(username, password, football_team, mlb_team):
@@ -70,6 +70,14 @@ def authenticate_user(username, password):
                 return True, football_team, mlb_team
     return False, None, None
 
+def is_user_exists(username):
+    db_file = "users_info.db"
+    with sqlite3.connect(db_file) as conn:
+        cursor = conn.cursor()
+        cursor.execute("SELECT username FROM users WHERE username=?", (username,))
+        existing_username = cursor.fetchone()
+        return existing_username is not None
+
 @app.route("/", methods=["GET", "POST"])
 def index():
     return render_template("index.html")
@@ -96,7 +104,10 @@ def signup():
         password = request.form["password"]
         football_team_input = request.form["football_team"]
         mlb_team_input = request.form["mlb_team"]
-        
+
+        if is_user_exists(username):
+            return render_template("signup.html", message="Username already exists. Please choose a different username.")
+
         football_team_info = fetch_team_info(football_team_input, "Premier League", api_key)
         if not football_team_info:
             return render_template("signup.html", message="Invalid football team. Please try again with a team from the Premier League.")
@@ -107,11 +118,15 @@ def signup():
 
         error_message = create_and_insert_user_data(username, password, football_team_info["team_name"], mlb_team_info["team_name"])
         if error_message:
-            return render_template("signup.html", message=error_message)
+            message = error_message  # Set the message in case of an error
+        else:
+            message = "Signup successful. Please login with your new account."
 
-        return redirect(url_for("login", message="Signup successful. Please login with your new account."))
+        if not error_message:
+            return redirect(url_for("login"))
 
-    return render_template("signup.html")
+    return render_template("signup.html", message=message)
+
 
 @app.route("/dashboard/<username>/<football_team>/<mlb_team>", endpoint="show_teams")
 def dashboard(username, football_team, mlb_team):
@@ -120,50 +135,61 @@ def dashboard(username, football_team, mlb_team):
 
     return render_template("dashboard.html", username=username, football_team=football_team_info, mlb_team=mlb_team_info)
 
-""" 
+
+"""
 class TestTeamInfo(unittest.TestCase):
-    def test_fetch_team_info_valid_team(self):
-        # Test fetching valid team info from the API for a generic team name in the Premier League
-        team_name = "Arsenal"
-        league = "Premier League"
-        api_key = "268d96d42db12c081079c5c0ee8bed74c2432e32cc88e1154a55a35640c9dbab"
-        team_info = fetch_team_info(team_name, league, api_key)
-        self.assertTrue(team_info, "Valid team info should be retrieved for a generic team in the Premier League.")
+    def setUp(self):
+        app.config["TESTING"] = True
+        self.app = app.test_client()
 
-    def test_fetch_team_info_invalid_team(self):
-        # Test fetching invalid team info from the API for an unknown team name in Premier League
-        team_name = "Invalid Team Name"
-        league = "Premier League"
-        api_key = "268d96d42db12c081079c5c0ee8bed74c2432e32cc88e1154a55a35640c9dbab"
-        team_info = fetch_team_info(team_name, league, api_key)
-        self.assertFalse(team_info, "Invalid team info should not be retrieved for an unknown team name in Premier League.")
+    def test_index_page(self):
+        response = self.app.get("/")
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b"Welcome To My Website", response.data)
 
-    def test_fetch_team_info_valid_mlb_team(self):
-        # Test fetching valid team info from the API for a generic team name in MLB
-        team_name = "New York Yankees"
-        league = "MLB"
-        api_key = "268d96d42db12c081079c5c0ee8bed74c2432e32cc88e1154a55a35640c9dbab"
-        team_info = fetch_team_info(team_name, league, api_key)
-        self.assertTrue(team_info, "Valid team info should be retrieved for a generic team in MLB.")
+    def signup(self, username, password, football_team, mlb_team):
+        return self.app.post("/signup", data={
+            "username": username,
+            "password": password,
+            "football_team": football_team,
+            "mlb_team": mlb_team
+        }, follow_redirects=True)
 
-    def test_fetch_team_info_invalid_mlb_team(self):
-        # Test fetching invalid team info from the API for an unknown team name in MLB
-        team_name = "Invalid Team Name"
-        league = "MLB"
-        api_key = "268d96d42db12c081079c5c0ee8bed74c2432e32cc88e1154a55a35640c9dbab"
-        team_info = fetch_team_info(team_name, league, api_key)
-        self.assertFalse(team_info, "Invalid team info should not be retrieved for an unknown team name in MLB.")
+    def login(self, username, password):
+        return self.app.post("/login", data={
+            "username": username,
+            "password": password
+        }, follow_redirects=True)
 
-    def test_is_user_exists_existing(self):
-        # Test if an existing username is detected
-        existing_username = "bob"
-        self.assertTrue(is_user_exists(existing_username), "Existing user should be detected.")
+    def test_signup_existing_user(self):
+        response = self.signup("bob", "password123", "Arsenal", "Yankees")
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b"Username already exists. Please choose a different username.", response.data)
 
-    def test_is_user_exists_new(self):
-        # Test if a new username is not detected
-        new_username = "new_user"
-        self.assertFalse(is_user_exists(new_username), "New user should not be detected.")"""
+    def test_signup_invalid_football_team(self):
+        response = self.signup("alice", "password123", "InvalidTeam", "Yankees")
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b"Invalid football team. Please try again with a team from the Premier League.", response.data)
+
+    def test_signup_invalid_mlb_team(self):
+        response = self.signup("charlie", "password123", "Arsenal", "InvalidTeam")
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b"Invalid MLB team. Please try again with a valid MLB team.", response.data)
+
+    def test_login_invalid_credentials(self):
+        response = self.login("unknown", "wrongpassword")
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b"Invalid username or password.", response.data)
+
+    def test_dashboard_authenticated(self):
+        response = self.app.get("/dashboard/bob/Arsenal/New%20York%20Yankees")
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b"Welcome, bob!", response.data)
+        self.assertIn(b"Team Name: Arsenal", response.data)
+        self.assertIn(b"Team Name: New York Yankees", response.data) """
+
 
 if __name__ == "__main__":
     api_key = "268d96d42db12c081079c5c0ee8bed74c2432e32cc88e1154a55a35640c9dbab"
+    #unittest.main()
     app.run(debug=True)
